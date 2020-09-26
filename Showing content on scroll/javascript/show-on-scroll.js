01e762ad03d07fbbbf7ee702ec391d3241fab3d2
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2020-09-26 15:57:34 +0530

    commit

--- Showing content on scroll/javascript/show-on-scroll.js
+++ Showing content on scroll/javascript/show-on-scroll.js
@@ -1,28 +1,33 @@
 /* window.addEventListener('scroll', function () {
     console.log('Scrolling...')
 }) */
-var scroll = window.requestAnimationFrame || function (callback) {
-    window.setTimeout(callback, 1000 / 60)
-};
 
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+//runn it.
 
 function loop() {
-    elementsToShow.forEach(function (element) {
-        if (isElementInViewport(element)) {
-            element.classList.add('isVisible')
-        } else {
-            element.classList.remove('isVisible')
-        }
+    document.querySelectorAll(".show-on-scroll").forEach(function (element) {
+      if (isElementInViewport(element)) {
+        element.classList.add("isVisible");
+      } else {
+        element.classList.remove("isVisible");
+      }
     });
-    scroll(loop)
+    scroll(loop)//scroll function works when page scrolls.
+};
+
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    //rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
 }
+
+var scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
 loop();
-
-function isElementInViewport(elm) {
-
-}
-
-function requestAnimationFrame() {
-
-}
