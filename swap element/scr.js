aba6ff5b1284e20924afc79156d20c0cd046de6c
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-08-14 13:27:59 +0530

    Added more useful and innovative Related to drag and drop.

---
+++ swap element/scr.js
@@ -0,0 +1,16 @@
+function cc(){
+	let ul = document.querySelectorAll('.cc')[0];
+	let newEl = document.createElement("li");
+	newEl.innerHTML = "fsd";
+	ul.append(newEl);
+}
+function cc2(){
+	let ul = document.querySelectorAll('.cc')[0];
+	let newEl = document.querySelectorAll('ul li')[0]; 
+	ul.append(newEl);
+}
+function cc3(){
+	let ul = document.querySelectorAll('.cc')[0];
+	let newEl = document.querySelectorAll('.nm')[0]; 
+	ul.append(newEl);
+}
