2b9a7ed03aa8a2d6e9e7b15396a764a6a5b716b8
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-09-03 23:10:22 +0530

    Html 5 new dialog box eg for modals.

---
+++ In-build-Dialog/js.js
@@ -0,0 +1,28 @@
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const selectEl = favDialog.querySelector("select");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+// Opens the <dialog> modally
+document.getElementById("showDialog").addEventListener("click", () => {
+	favDialog.showModal();
+});
+
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener("change", (e) => {
+	confirmBtn.value = selectEl.value;
+});
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+favDialog.addEventListener("close", (e) => {
+	outputBox.value =
+		favDialog.returnValue === "default"
+			? "No return value."
+			: `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+	event.preventDefault(); // We don't want to submit this fake form
+	favDialog.close(selectEl.value); // Have to send the select box value here.
+});
