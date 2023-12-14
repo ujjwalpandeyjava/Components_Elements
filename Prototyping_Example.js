f99eac03ef3ffeac7008b6a8c451e8112da790f7
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-12-14 11:56:52 +0530

    multiple additions and updated from last commit.

---
+++ Prototyping_Example.js
@@ -0,0 +1,17 @@
+﻿function Rectangle(height, width) {
+  this.area = () => { return height * width };
+  this.perimeter = () => { return 2 * (height + width) };
+}
+
+//  X, Y plain distance
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+  this.distanceFrom = function (another) {
+    return Math.sqrt(Math.pow((this.x - another.x), 2) + Math.pow((this.y - another.y), 2)); // a^2 + b^2
+  }
+}
+
+String.prototype.isPalindrome = function () {
+  return this.split('').reverse().join('') == this; // with a == operator we don't need to use the valueOf method. WHY?? Check the type of this.split('').reverse().join('')
+}
--- double-click-heart/script.js
+++ double-click-heart/script.js
@@ -5,15 +5,14 @@ let clickTime = 0
 let timesClicked = 0
 
 loveMe.addEventListener('click', (e) => {
-    if(clickTime === 0) {
+    if (clickTime === 0)
         clickTime = new Date().getTime()
-    } else {
-        if((new Date().getTime() - clickTime) < 800) {
+    else {
+        if ((new Date().getTime() - clickTime) < 800) {
             createHeart(e)
             clickTime = 0
-        } else {
+        } else
             clickTime = new Date().getTime()
-        }
     }
 })
 
@@ -21,7 +20,7 @@ const createHeart = (e) => {
     const heart = document.createElement('i')
     heart.classList.add('fas')
     heart.classList.add('fa-heart')
-
+    heart.innerHTML = "fe"
     const x = e.clientX
     const y = e.clientY
 
